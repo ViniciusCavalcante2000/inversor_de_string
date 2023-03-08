6ba703dd64e491ef8f73b3d4bddb8de909118694
Vinícius do Nascimento Cavalcante <viniciuscavalcanterj@gmail.com>
2023-03-08 11:57:17 -0300

    Crio um arquivo js que inverte strings

---
+++ inversor_string.js
@@ -0,0 +1,8 @@
+let strinInicial = "Eu amo programação";
+let stringInvertida = "";
+
+for (let i = strinInicial.length - 1; i >= 0; i--) {
+  stringInvertida += strinInicial[i];
+}
+
+console.log(stringInvertida);
